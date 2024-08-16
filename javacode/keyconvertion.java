0b9a73ec81f86cd515f8b0769364dde7260a3fae
hephaestusboy <mathewalan414@gmail.com>
2024-08-16 23:21:40 +0530

    fixed logical error on keyconvertion.java

--- javacode/keyconvertion.java
+++ javacode/keyconvertion.java
@@ -21,32 +21,21 @@ public class keyconvertion {
         String keyString2 = Integer.toString(randomkey);
         StringBuilder keyString = new StringBuilder();
         int j=0,k=0;
-        for(int i=0;i<(keyString1.length()+keyString2.length());i++)
+        for (int i = 0; i < (keyString1.length() + keyString2.length()); i++) 
         {
-            if(j<keyString1.length() && k<keyString2.length())
+            if (j < keyString1.length() && (i % 2 == 0 || k >= keyString2.length())) 
             {
-                if(i%2==0 && j<keyString1.length())
-                {
-                    keyString.append(keyString1.charAt(j));
-                    j++;
-                }
-                if(i%2==1 && k<keyString2.length())
-                {
-                    keyString.append(keyString2.charAt(k));
-                    k++;
-                }
-            }
-            if(j==keyString1.length())
+                keyString.append(keyString1.charAt(j));
+                j++;
+            } 
+            else if (k < keyString2.length()) 
             {
                 keyString.append(keyString2.charAt(k));
                 k++;
             }
-            if(k==keyString2.length())
-            {
-                keyString.append(keyString1.charAt(j));
-                j++;
-            }
+         
         }
+
         String key = keyString.toString();
         System.out.println("Converted key is "+stkeyconverted+"\n KeyString1 is "+keyString1+"\n Keystring2 is "+keyString2+"\n Key is "+key);
         // return new Pair<long,Integer>(stkeyconverted,keybackup);
